5342ce4e35fabc495dec6ca61c52be7254557828
CarstenKost <bio15cko@mail.lu.se>
2018-10-11 15:10:17 +0200

    added automate_run.bash, script in bash

---
+++ automate_run.bash
@@ -0,0 +1,13 @@
+#Define variables
+RESULT_DIR=/c/Users/carst/Documents/data-shell/results
+
+INPUT_DIR=/c/Users/carst/Documents/data-shell/molecules
+
+rm -rf $RESULTS_DIR
+mkdir $RESULTS_DIR
+for filename in molecules/*.pdb
+do
+wc -l $filename | sort -n | head -n 1 >>$RESULTS_DIR/length.txt
+done
+
+cat  $RESULTS_DIR/length.txt
